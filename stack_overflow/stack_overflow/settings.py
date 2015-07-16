3370946a3e32f7a9a93f05ae6c89766913db6123
Lance Rogers <lancekincaid1994@gmail.com>
2015-07-16 16:28:14 -0400

    set up postgres database

--- stack_overflow/stack_overflow/settings.py
+++ stack_overflow/stack_overflow/settings.py
@@ -76,10 +76,12 @@ WSGI_APPLICATION = 'stack_overflow.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'atus',    #'db_name',
+        'USER': 'atus',   #'db_user',
     }
 }
+
 
 
 # Internationalization
