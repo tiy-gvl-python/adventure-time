30885961457d86f4ed93ba9927b3dab420b54700
Bekk Blando <bekkblando@gmail.com>
2015-07-16 15:53:34 -0400

    Setting up

--- autus_api/autus_api/settings.py
+++ autus_api/autus_api/settings.py
@@ -76,11 +76,11 @@ WSGI_APPLICATION = 'autus_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'atus',
+        'USER': 'BekkBlando',
     }
 }
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
