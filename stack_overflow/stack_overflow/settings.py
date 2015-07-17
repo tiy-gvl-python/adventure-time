9efcecc9c3c8fcec3582c8fc7c80309f12236ffb
Lance Rogers <lancekincaid1994@gmail.com>
2015-07-17 11:58:30 -0400

    undid staticfile fix

--- stack_overflow/stack_overflow/settings.py
+++ stack_overflow/stack_overflow/settings.py
@@ -103,10 +103,3 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = 'staticfiles'
-STATIC_URL = '/static/'
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
