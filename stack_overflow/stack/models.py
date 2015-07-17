a6c57001d1d583fb3549877eb827370b21d0c7e0
Lance Rogers <lancekincaid1994@gmail.com>
2015-07-16 23:57:29 -0400

    I have most of my models created

--- stack_overflow/stack/models.py
+++ stack_overflow/stack/models.py
@@ -4,19 +4,41 @@ from django.db import models
 # Create your models here.
 
 class Question(models.Model):
-    user = models.ManyToManyField(User)
+    user = models.ForeignKey(User)
     title = models.CharField(max_length=30)
     text = models.TextField()
+    slug = models.SlugField()
+    score = models.IntegerField()
+    tags = models.ManyToManyField('Tag')
 
+    def __str__(self):
+        return 'User: {}\nTitle: {}'.format(self.user.username, self.title)
 
 class Answers(models.Model):
-    pass
+    user = models.ForeignKey(User)
+    text = models.TextField()
+    score = models.IntegerField()
+
+
+
+class Tag(models.Model):
+    tag = models.CharField(max_length=45) #length of longest english word
 
 
 class Vote(models.Model):
-    pass
+    upvote = models.BooleanField()
+    downvote = models.BooleanField()
+
+    def __str__(self):
+        if self.upvote == True:
+            return 'Upvote'
+        elif self.downvote == True:
+            return 'Downvote'
+        else:
+            return 'No vote'
 
 
 class Profile(models.Model):
-    pass
+    user = models.OneToOneField(User)
+    points = models.IntegerField(default=0)
 
