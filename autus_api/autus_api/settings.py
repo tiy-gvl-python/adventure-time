dc8bfee4c0d0218cc834e4c0b65274773ce12832
Bekk Blando <bekkblando@gmail.com>
2015-07-17 00:19:26 -0400

    Heroku

--- autus_api/autus_api/settings.py
+++ autus_api/autus_api/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-
+import dj_database_url
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
@@ -75,13 +75,15 @@ WSGI_APPLICATION = 'autus_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
-DATABASES = {
+"""DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'atus',
         'USER': 'BekkBlando',
     }
-}
+}"""
+
+DATABASES = {'default': dj_database_url.config(default='postgres://localhost')}
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
