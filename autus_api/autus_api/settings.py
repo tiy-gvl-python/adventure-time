a88b1817e6bd80b12314e5d973b55d55b8cdf437
Bekk Blando <bekkblando@gmail.com>
2015-07-16 22:16:40 -0400

    removed url pharse

--- autus_api/autus_api/settings.py
+++ autus_api/autus_api/settings.py
@@ -101,8 +101,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 # Parse database configuration from $DATABASE_URL
-import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
+
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
@@ -111,7 +110,7 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 ALLOWED_HOSTS = ['*']
 
 # Static asset configuration
-import os
+
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
