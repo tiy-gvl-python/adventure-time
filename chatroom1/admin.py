44dd9d7482baf09bddd70aa3d4fa6e8ce13c858c
Jeff Hacker <reachjkh@yahoo.com>
2015-07-18 15:24:13 -0400

    models built and  added to admin

--- chatroom1/admin.py
+++ chatroom1/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
 # Register your models here.
+from .models import UserProfile, ConversationThread, PostReply
+
+
+admin.site.register(UserProfile)
+admin.site.register(ConversationThread)
+admin.site.register(PostReply)
+
+'''
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+    verbose_name_plural = 'profiles'
+   # fk_name = 'user'
+
+class UserAdmin(UserAdmin):
+    inlines = (ProfileInline, )
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff')
+    list_filter = ('is_staff', 'is_superuser', 'is_active')
+# modeled after stack overflow
+admin.site.unregister(User)  # Unregister user to add new inline ProfileInline
+admin.site.register(User, UserAdmin)  # Register User with this inline profile
+'''
--- chatroom1/models.py
+++ chatroom1/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, null=True)
+    reputation = models.IntegerField(default=0)
+    member = models.BooleanField(default=False)
+    moderator = models.BooleanField(default=False)
+    administrator = models.BooleanField(default=False)
+
+    def __str__(self):
+        return 'username: {}, reputation: {}'.format(self.user, self.reputation)
+'''
+    class Meta:
+        verbose_name = 'id'
+        verbose_name_plural = 'id'
+
+    def __str__(self):
+        return "{}".format(self.user)
+        '''
+
+
+class ConversationThread(models.Model):
+    user = models.ForeignKey(User)
+    title = models.CharField(max_length=50)
+    body = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return 'username: {}'.format(self.user)
+
+
+class PostReply(models.Model):
+    user = models.ForeignKey(User)
+    body = models.TextField()
+    thread = models.ForeignKey(ConversationThread)
+    post_rating = models.IntegerField(default=0)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return 'username: {}, reputation: {}'.format(self.user, self.reputation)
+
+    class Meta:
+        ordering = ["-timestamp"]
