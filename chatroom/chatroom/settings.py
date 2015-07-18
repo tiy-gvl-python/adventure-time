99759aa679ae589244ff4ba7a3a92ada1acfce6c
Jeff Hacker <reachjkh@yahoo.com>
2015-07-17 21:43:26 -0400

    corrected settings.py

--- chatroom/chatroom/settings.py
+++ chatroom/chatroom/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'chatroom1',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -78,7 +79,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'chatroom',
-        'USER': 'chatroom'
+        'USER': 'chatroom',
     }
 }
 
