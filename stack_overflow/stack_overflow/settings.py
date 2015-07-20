38edcee07c55a1da24e36ca1c99c69b19281dc3b
Lance Rogers <lancekincaid1994@gmail.com>
2015-07-19 22:14:14 -0400

    ...

--- stack_overflow/stack_overflow/settings.py
+++ stack_overflow/stack_overflow/settings.py
@@ -104,4 +104,3 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
--- stack_overflow/stack_overflow/wsgi.py
+++ stack_overflow/stack_overflow/wsgi.py
@@ -10,7 +10,6 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
 
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "stack_overflow.settings")
@@ -21,5 +20,4 @@ from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
 
 application = Cling(get_wsgi_application())
-application = DjangoWhiteNoise(application)
 
