2c02e306c04303d49d094e41bc491e07ff85c1ba
Lance Rogers <lancekincaid1994@gmail.com>
2015-07-28 23:57:51 -0400

    fixed voting to allow only one vote per user per object being voted on

--- stack_overflow/stack/views.py
+++ stack_overflow/stack/views.py
@@ -167,64 +167,62 @@ class TagCreation(CreateView):
 
 
 def vote_create(request, votee_pk, model_type, vote_type='upvote'):
-    print('here')
-    x_var = votee_pk
+    print('here') # separate this out into more than one function so that
+    x_var = votee_pk    # people can only vote on one question at a time
     if request.POST:
         print('sent post')
         user_pk = request.user.pk
-        question = False
-        answer = False
-        downvote = False
-        upvote = True
         profile = Profile.objects.get(pk=user_pk)
-        model_type = model_type
-        obj = ''
-        if model_type == 'question':
-            question = True
-            obj = Question.objects.get(pk=votee_pk)
-        if model_type == 'answer':
-            answer = True
-            obj = Answers.objects.get(pk=votee_pk)
-            x_var = obj.question.pk
-        if vote_type == 'downvote':
-            downvote = True
-            upvote = False
-            owner = Profile.objects.get(pk=obj.user.pk)
-            obj.score -= 5
-            obj.save()
-            if profile.score > 0:
-                profile.score -= 1
+        if not Vote.objects.filter(votee_pk=votee_pk).filter(voter=profile):
+            question = False
+            answer = False
+            downvote = False
+            upvote = True
+            profile = Profile.objects.get(pk=user_pk)
+            model_type = model_type
+            obj = ''
+            if model_type == 'question':
+                question = True
+                obj = Question.objects.get(pk=votee_pk)
+            if model_type == 'answer':
+                answer = True
+                obj = Answers.objects.get(pk=votee_pk)
+                x_var = obj.question.pk
+            if vote_type == 'downvote':
+                downvote = True
+                upvote = False
+                owner = Profile.objects.get(pk=obj.user.pk)
+                obj.score -= 5
+                obj.save()
+                if profile.score > 0:
+                    profile.score -= 1
+                    profile.save()
+                if owner.score > 5:
+                    owner.score -= 5
+                    owner.save()
+            else:
+                numset = '000000001'
+                if profile.score < 25:
+                    numset = '010021'
+                rand = random.choice(numset)
+                owner = Profile.objects.get(pk=obj.user.pk)
+                owner.score += 10
+                print('It works here is the owners score:', owner.score)
+                profile.score += int(rand)
                 profile.save()
-            if owner.score > 5:
-                owner.score -= 5
+                obj.score += 10
+                obj.save()
                 owner.save()
+            vote = Vote.objects.create(votee_pk=votee_pk,
+                                       voter=profile,
+                                       upvote=upvote,
+                                       downvote=downvote,
+                                       completed=True,
+                                       is_question=question,
+                                       is_answer=answer)
+            vote.save()
         else:
-            numset = '000000001'
-            if profile.score < 25:
-                numset = '010021'
-            rand = random.choice(numset)
-            owner = Profile.objects.get(pk=obj.user.pk)
-            owner.score += 10
-            print('It works here is the owners score:', owner.score)
-            profile.score += int(rand)
-            profile.save()
-            obj.score += 10
-            obj.save()
-            owner.save()
-        vote = Vote.objects.create(votee_pk=votee_pk,
-                                   voter=profile,
-                                   upvote=upvote,
-                                   downvote=downvote,
-                                   completed=True,
-                                   is_question=question,
-                                   is_answer=answer)
-        vote.save()
+            pass
         return redirect('stack:question_page', question_id=x_var)
     else:
        return redirect('stack:question_page', question_id=x_var)
-
-
-
-
-
-
