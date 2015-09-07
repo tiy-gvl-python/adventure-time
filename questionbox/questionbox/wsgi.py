8483cd4604b4c0eceb4a08cfb302f013c4a64751
Ubuntu <ubuntu@ip-172-31-26-112.us-west-2.compute.internal>
2015-09-07 21:42:34 +0000

    changed src for aws to work

--- questionbox/questionbox/wsgi.py
+++ questionbox/questionbox/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "questionboxsrc.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "questionbox.settings")
 
 application = get_wsgi_application()
